8e771265f71fb71760690865e6a1b6bea2fbb193
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-09-25 17:46:09 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,6 @@
     import Button from "../lib/components/Button.svelte";
 </script>
 <h1 class="text-3xl mb-4 dark:text-grey">
-    Simeon's List
+    Susanna's List
 </h1>
 <Button type="link" href="/list">Go to the list</Button>
